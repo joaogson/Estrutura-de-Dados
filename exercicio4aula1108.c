08caa8774ce7007089ba1cfec79e56a531f4f39f
joaogson <joaogsonalio@gmail.com>
2023-08-11 22:17:21 -0300

    Aula 11/08
    Leia uma matriz 6 x 6, conte e escreva quais e quantos valores maiores que 10 ela possui.

---
+++ exercicio4aula1108.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+main(){
+    int tamanho = 6;
+    int matriz[tamanho][tamanho], cont;
+
+
+    for(int linha = 0; linha<tamanho; linha++){
+        for(int coluna = 0;coluna<tamanho; coluna++){
+            matriz[linha][coluna] = rand() % 20;
+        }
+    }
+
+    for(int linha = 0; linha<tamanho; linha++){
+        for(int coluna = 0;coluna<tamanho; coluna++){
+            printf("[%2d]", matriz[linha][coluna]);
+        }
+        printf("\n");
+        
+    }
+    printf("\n");
+    printf("\n");
+
+    for(int linha = 0; linha<tamanho; linha++){
+        for(int coluna = 0;coluna<tamanho; coluna++){
+            if(matriz[linha][coluna]>10){
+                printf("[%2d]", matriz[linha][coluna]);
+                cont++;
+            }
+            else{
+                printf("[  ]");
+            }
+        }
+        printf("\n");
+    }
+    printf("\n");
+    printf("\n");
+    printf("A matriz possui [%2d] valores maiores que 10", cont);
+    printf("\n");
+    printf("\n");
+    
+}
